fba5b715958fae2050415ebbf857786b85afa6e7
Igor Sharonov <igsha@users.noreply.github.com>
2024-05-09 16:58:37 +0300

    Add nix flake ecosystem

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A tmux plugin collection";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    overlay = final: prev: {
+      tmuxPlugins = prev.tmuxPlugins // {
+        mycollection = prev.tmuxPlugins.mkTmuxPlugin {
+          pluginName = "mycollection";
+          version = "unstable-2024-05-01";
+          src = ./.;
+        };
+      };
+    };
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [ overlay ];
+    };
+  in {
+    overlays.default = overlay;
+    packages.x86_64-linux = {
+      tmuxPlugins.mycollection = pkgs.tmuxPlugins.mycollection;
+      default = self.packages.x86_64-linux.tmuxPlugins.mycollection;
+    };
+  };
+}
